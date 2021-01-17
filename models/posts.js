abc661e741608261746153e9453966598e050843
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-17 11:44:56 +0200

    Edit Schemas 1

--- models/posts.js
+++ models/posts.js
@@ -1,2 +1,49 @@
 const mongoose = require('mongoose');
 
+
+const PostSchema = mongoose.Schema({
+    userID: {
+        type: String
+    },
+    titleUnit: {
+        type: String,
+        required: true
+    },
+    locationUnit:{
+        type: String,
+        required: true
+    },
+    typeUnit:{
+        type: String,
+        required: true
+    },
+    guestsUnit:{
+        type: Number,
+        required: true
+    },
+    bedroomsUnit:{
+        type: Number,
+        required: true
+    },
+    bathroomsUnit:{
+        type: Number,
+        required: true
+    },
+    amenitiesUnit:{
+        type: String,
+        required: true
+    },
+    ratingUnit: {
+        type: Number
+    },
+    descriptionUnit:  {
+        type: String
+    },
+    reviewsAtUnit: {
+        type: String
+    },
+    imagesRentalUnit: {
+        type: String
+    }
+})
+
--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
 const PostSchema = mongoose.Schema({
-    userID: {
+    profilePic:  {
         type: String
     },
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
-        type: String
+        type: String,
+        required: true
     },
     email: {
-        type: String
+        type: String,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     rating: {
         type: Number
