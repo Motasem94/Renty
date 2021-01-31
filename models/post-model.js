7ef72cb22151a6b37b2dae0bd2a59cad6a8822f0
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-31 16:23:41 +0200

    Adding date to user & post models

--- models/post-model.js
+++ models/post-model.js
@@ -1,50 +1,53 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const PostSchema = mongoose.Schema({
-    userID: {
-        type: String
-    },
-    titleUnit: {
-        type: String,
-        required: true
-    },
-    locationUnit:{
-        type: String,
-        required: true
-    },
-    typeUnit:{
-        type: String,
-        required: true
-    },
-    guestsUnit:{
-        type: Number,
-        required: true
-    },
-    bedroomsUnit:{
-        type: Number,
-        required: true
-    },
-    bathroomsUnit:{
-        type: Number,
-        required: true
-    },
-    amenitiesUnit:{
-        type: String,
-        required: true
-    },
-    ratingUnit: {
-        type: Number
-    },
-    descriptionUnit:  {
-        type: String
-    },
-    reviewsAtUnit: {
-        type: String
-    },
-    imagesRentalUnit: {
-        type: String
-    }
-})
+  userID: {
+    type: String,
+  },
+  titleUnit: {
+    type: String,
+    required: true,
+  },
+  locationUnit: {
+    type: String,
+    required: true,
+  },
+  typeUnit: {
+    type: String,
+    required: true,
+  },
+  guestsUnit: {
+    type: Number,
+    required: true,
+  },
+  bedroomsUnit: {
+    type: Number,
+    required: true,
+  },
+  bathroomsUnit: {
+    type: Number,
+    required: true,
+  },
+  amenitiesUnit: {
+    type: String,
+    required: true,
+  },
+  ratingUnit: {
+    type: Number,
+  },
+  descriptionUnit: {
+    type: String,
+  },
+  reviewsAtUnit: {
+    type: String,
+  },
+  imagesRentalUnit: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
--- models/user-model.js
+++ models/user-model.js
@@ -1,44 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profilePic:  {
-        type: String
-    },
-    rating: {
-        type: Number
-    },
-    about:  {
-        type: String
-    },
-    languages: {
-        type: String
-    },
-    reviewsByUser: {
-        type: String
-    },
-    reviewsAtUser: {
-        type: String
-    },
-    address: {
-        type: String
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+  },
+  about: {
+    type: String,
+  },
+  languages: {
+    type: String,
+  },
+  reviewsByUser: {
+    type: String,
+  },
+  reviewsAtUser: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
