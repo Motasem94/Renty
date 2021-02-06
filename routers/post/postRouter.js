dfb37cf7d0c404a5f30ebd5098617b630c16c381
Motasem94 <amotasem.ahmed@gmail.com>
2021-02-06 18:19:46 +0200

    Chang status of post (pendening/approve/reject)

--- routers/post/postRouter.js
+++ routers/post/postRouter.js
@@ -5,10 +5,17 @@ const isAuth = require("../../middlewares/isAuth");
 const isAdmin = require("../../middlewares/isAdmin");
 
 router.post("/post/create", isAuth, PostCtrl.CreatePost);
-router.get("/post/all", isAuth, PostCtrl.GetAllPosts);
+router.get("/post/all", isAuth, isAdmin, PostCtrl.GetAllPosts);
 router.get("/post/:id", isAuth, PostCtrl.GetPost);
 router.patch("/post/update/:id", isAuth, PostCtrl.UpdatePost);
 router.delete("/post/delete/:id", isAuth, PostCtrl.DeletePost);
 router.patch("/post/image-upload/:id", isAuth, PostCtrl.UploadImage);
+router.patch(
+  "/post/pendeing-action/:id",
+  isAuth,
+  isAdmin,
+  PostCtrl.UpdatePostStatus
+);
+router.get("/post/all/approved", PostCtrl.GetAllApprovedPosts);
 
 module.exports = router;
