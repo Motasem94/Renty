d33198cb6c7e8b3c32069b2fe25d6ea6598e38d8
Motasem94 <amotasem.ahmed@gmail.com>
2021-02-09 23:20:50 +0200

    feed back skeleton

---
+++ controllers/feedBack/feedBack-ctrl.js
@@ -0,0 +1,22 @@
+const FeedBack = require("../../models/feedBack-model");
+
+exports.CreateFeedBack = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.GetFeedBack = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.DeleteFeedBack = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ models/feedBack-model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const FeedBackSchema = mongoose.Schema(
+  {
+    rate: {
+      type: Number,
+    },
+    review: {
+      type: String,
+    },
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    postID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("FeedBack", FeedBackSchema);
