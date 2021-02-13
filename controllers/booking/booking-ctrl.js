244b4d4a8a6bc327331305c03de7f36ce0f81262
Motasem94 <amotasem.ahmed@gmail.com>
2021-02-13 02:50:30 +0200

    pagination has been added

--- controllers/booking/booking-ctrl.js
+++ controllers/booking/booking-ctrl.js
@@ -47,10 +47,30 @@ exports.CreateBooking = async (req, res) => {
 exports.GetAllBooking = async (req, res) => {
   try {
     const booking = await Booking.find();
+
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const paginateBookings = {};
+
+    if (endIndex < booking.length) {
+      paginateBookings.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+    if (startIndex > 0) {
+      paginateBookings.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    paginateBookings.bookings = booking.slice(startIndex, endIndex);
     res.status(200).json({
       Message: "Bookings fetched successfully",
-      NumberOfBookings: booking.length,
-      booking,
+      NumberOfAllBookings: booking.length,
+      paginateBookings,
     });
   } catch (error) {
     console.log(error);
